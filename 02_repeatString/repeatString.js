cd0b4f832ba247956c702f76153a43063dd7d7d8
hellojquinn <hello.jquinn@gmail.com>
2022-12-03 17:20:25 +1100

    Repeat string function for javascript exercise with TOP

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+function repeatString(string, num) {
 
+let repeatedStr = "";
+
+while (num > 0) {
+    repeatedStr += string;
+    num--; 
+    } 
+    if (num == -1)
+        return "ERROR";
+    else {
+        return repeatedStr;
+ }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
