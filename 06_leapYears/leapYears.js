14f990cb503fb1857e25f6b641499883f5462c96
hellojquinn <hello.jquinn@gmail.com>
2022-12-06 06:10:03 +1100

    Leap Years Solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if (year % 400 === 0) {
+        return true;
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    return year % 4 === 0;
 
 };
 
