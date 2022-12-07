b646e8d62a23867d802b3b23947055b5ab3c0571
hellojquinn <hello.jquinn@gmail.com>
2022-12-08 07:00:27 +1100

    Palindromes Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+const cleanString = string.toLowerCase().replace(/[^a-z]/g, "");
+const letters = cleanString.split("");
+const reversed = letters.reverse();
+const reversedString = reversed.join("");
+
+return cleanString === reversedString;
 };
 
 // Do not edit below this line
