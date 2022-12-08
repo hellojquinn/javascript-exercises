4fda835b9b8d0f425ddc31fc30c78d88c5fdb9ac
hellojquinn <hello.jquinn@gmail.com>
2022-12-09 07:06:09 +1100

    Fibonacci Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+    if (input <= 0) {
+        return "OOPS";
+    } else if (input == "" || input) {
+        
+    let fib = [0, 1];
+    for (let i = 2; i <= input; i++) {
+        fib[i] = fib[i - 1] + fib[i -2];
+    }
+    return fib[input];
+}
 };
 
 // Do not edit below this line
